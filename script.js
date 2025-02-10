10a8c8f43239dfa79e633bb2be7115b42fe7cbfa
woodyistaken <woodyaung2005@gmail.com>
2025-02-10 00:05:27 -0800

    Add UI

--- script.js
+++ script.js
@@ -20,84 +20,74 @@ function getComputerChoice(){
     }
 }
 
-//Get the choice of player
-function getHumanChoice(){
-    //get player input
-    let input=prompt("Rock Paper Scissors")
-    //make input all lowercase
-    input=input.toLowerCase();
-    //return input
-    return input;
-}
 
 
 
 //Start the game
-function playGame(){
+function playGame(human){
     //Play a round of game with computer and human choices
     function playRound(humanChoice,computerChoice){
         //make sure both choices are lowercase
         humanChoice=humanChoice.toLowerCase();
         computerChoice=computerChoice.toLowerCase();
-
         //if there is a draw
-        if(humanChoice===computerChoice){
+        if(humanChoice==computerChoice){
             //print out "Draw, both chose humanChoice"
-            console.log(`Draw, both chose ${humanChoice}`);
+            resultDisplay.innerHTML=`Draw, both chose ${humanChoice}`;
             
         }
         //if human player choses rock
-        else if(humanChoice==="rock")
+        else if(humanChoice=="rock")
         {
             //if computer chooses paper
-            if(computerChoice==="paper"){
+            if(computerChoice=="paper"){
                 //increment computer score by one
                 computerScore++;
                 //print out "You lose. computerChoice beats humanChoice"
-                console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+                resultDisplay.textContext=`You lose. ${computerChoice} beats ${humanChoice}`
             }
             //if computer chooses scissors
-            else if(computerChoice==="scissors"){
+            else if(computerChoice=="scissors"){
                 //increment human score by one
                 humanScore++;
                 //print out "You win. humanChoice beats computerChoice"
-                console.log(`You win. ${humanChoice} beats ${computerChoice}`)
+                resultDisplay.textContext=`You win. ${humanChoice} beats ${computerChoice}`
             }
         }
         //if human player choses paper
-        else if(humanChoice==="paper")
+        else if(humanChoice=="paper")
             {
                 //if computer chooses scissors
                 if(computerChoice==="scissors"){
                     //increment computer score by one
                     computerScore++;
                     //print out "You lose. computerChoice beats humanChoice"
-                    console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+                    resultDisplay.textContext=`You lose. ${computerChoice} beats ${humanChoice}`
                 }
                 //if computer chooses rock
                 else if(computerChoice=="rock"){
                     //increment human score by one
                     humanScore++;
                     //print out "You win. humanChoice beats computerChoice"
-                    console.log(`You win. ${humanChoice} beats ${computerChoice}`)
+                    resultDisplay.textContext=`You win. ${humanChoice} beats ${computerChoice}`
                 }
             }
         //if human player choses scissors
-        else if(humanChoice==="scissors")
+        else if(humanChoice=="scissors")
             {
                 //if computer chooses rock
                 if(computerChoice==="rock"){
                     //increment computer score by one
                     computerScore++;
                     //print out "You lose. computerChoice beats humanChoice"
-                    console.log(`You lose. ${computerChoice} beats ${humanChoice}`)
+                    resultDisplay.textContext=`You lose. ${computerChoice} beats ${humanChoice}`
                 }
                 //if computer chooses paper
                 if(computerChoice==="paper"){
                     //increment human score by one
                     humanScore++;
                     //print out "You win. humanChoice beats computerChoice"
-                    console.log(`You win. ${humanChoice} beats ${computerChoice}`)
+                    resultDisplay.textContext=`You win. ${humanChoice} beats ${computerChoice}`
                 }
             }
     
@@ -111,11 +101,17 @@ function playGame(){
 
 
     //Playing the game
-    const humanSelection = getHumanChoice();
+    const humanSelection = human;
     const computerSelection = getComputerChoice();
+    console.log(humanSelection+" "+computerSelection)
     playRound(humanSelection, computerSelection);
 }
 
-for(let i=0; i<100;i++){
-    playGame();
-}
+const rock=document.querySelector("#rock");
+const paper=document.querySelector("#paper");
+const scissors=document.querySelector("#scissors");
+rock.addEventListener("click",(e)=>playGame("rock"))
+paper.addEventListener("click",(e)=>playGame("paper"))
+scissors.addEventListener("click",(e)=>playGame("scissors"))
+
+const resultDisplay=document.querySelector("#resultDisplay");
